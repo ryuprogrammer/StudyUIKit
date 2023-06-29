464a1a6597f5fe4c5de92b848148209af7bb4f1b
トム・クルーズ <ryumagic@tomukuruzunoMacBook-Pro.local>
2023-06-29 23:52:04 +0900

    ミュージックアプリのファイル追加

---
+++ MusicApp_Storyboard/MusicApp_Storyboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicApp_Storyboard
+//
+//  Created by トム・クルーズ on 2023/06/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
