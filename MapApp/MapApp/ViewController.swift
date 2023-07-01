0d72548fde3ad98704858474d2bf771cf4866c21
トム・クルーズ <ryu1421magic@icloud.com>
2023-07-01 18:27:58 +0900

    CLGeocoderで位置情報を取得

--- MapApp/MapApp/ViewController.swift
+++ MapApp/MapApp/ViewController.swift
@@ -21,5 +21,39 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var inputText: UITextField!
     
     @IBOutlet weak var dispMap: MKMapView!
+    
+    // TextFieldの入力完了したら通知を受け取る
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // キーボードを閉じる
+        textField.resignFirstResponder()
+        
+        // 入力された文字を取り出す
+        if let searchKey = textField.text {
+            print(searchKey)
+            
+            // CLGeocoderを取得
+            let geocoder = CLGeocoder()
+            
+            // 入力された文字から位置情報を取得
+            geocoder.geocodeAddressString(searchKey) { placemarks, error in
+                // 位置情報が存在する場合は、unwrapPlacemarksに取り出す
+                guard let unwrapPlacemarks = placemarks else { return }
+                
+                // １件目の情報を取り出す
+                guard let firstPlacemark = unwrapPlacemarks.first else { return }
+                
+                // 位置情報を取り出す
+                guard let location = firstPlacemark.location else { return }
+                
+                // 位置情報から緯度経度をtargetCoordinateに取り出す
+                let targetCoordinate = location.coordinate
+                
+                print(targetCoordinate)
+            }
+        }
+        
+        // デフォルト動作を行うのでtrueを返す
+        return true
+    }
 }
 
