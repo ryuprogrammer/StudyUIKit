cb238fa97d16c7dfaa65c7aa4006e9b51e75a67e
トム・クルーズ <ryumagic@tomukuruzunoMacBook-Pro.local>
2023-07-01 17:48:11 +0900

    ファイルを追加

---
+++ MapApp/MapApp/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MapApp
+//
+//  Created by トム・クルーズ on 2023/07/01.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        // Text
+    }
+
+    @IBOutlet weak var inputText: UITextField!
+    
+    @IBOutlet weak var dispMap: MKMapView!
+}
+
