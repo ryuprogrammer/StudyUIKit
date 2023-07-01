659b47a51be38df0a7fb0b1e0fa4e4b7d9a9a177
トム・クルーズ <ryumagic@tomukuruzunoMacBook-Pro.local>
2023-07-01 14:32:17 +0900

    音源再生メソッド追加

--- MusicApp_Storyboard/MusicApp_Storyboard/ViewController.swift
+++ MusicApp_Storyboard/MusicApp_Storyboard/ViewController.swift
@@ -6,12 +6,50 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    // ギター音源データの読み込み
+    let guitarData = NSDataAsset(name: "guitarSound")!.data
+    // ギター用プレーヤーの変数
+    var soundPlayer: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func guitar(_ sender: UIButton) {
+        soundPlay(data: guitarData)
+    }
+    
+    @IBAction func cymbal(_ sender: UIButton) {
+    }
+    
+    @IBAction func playSound(_ sender: UIButton) {
+    }
+    
+    @IBAction func stopSound(_ sender: UIButton) {
+    }
+    
+    func soundPlay(data: Data) {
+        do {
+            soundPlayer = try AVAudioPlayer(data: data)
+            soundPlayer.play()
+        } catch {
+            print("プレーヤーでエラー")
+        }
+    }
+    
+//    // 共通のプレーヤー再生処理
+//    fileprivate func soundPlayer(player:inout AVAudioPlayer, path: URL, count: Int) {
+//        do {
+//            player = try AVAudioPlayer(contentsOf: path, fileTypeHint: nil)
+//            player.numberOfLoops = count
+//            player.play()
+//        } catch {
+//            print("player is error: \(error)")
+//        }
+//    }
 }
-
