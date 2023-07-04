ed3f269a51cee021d7278121438a4ccab8b22676
トム・クルーズ <ryu1421magic@icloud.com>
2023-07-04 13:24:48 +0900

    PickerViewを作成

--- TimerApp/TimerApp/SettingViewController.swift
+++ TimerApp/TimerApp/SettingViewController.swift
@@ -7,12 +7,32 @@
 
 import UIKit
 
-class SettingViewController: UIViewController {
+class SettingViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    // UIPickerViewに表示するデータをArrayで作成
+    let settingArray: [Int] = [10, 20, 30, 40, 50, 60]
+    // 設定値を覚えるキーを設定
+    let settingKey = "timer_value"
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        // timerSettingPickerのデリゲートとデータソースの通知先を指定
+        timerSettingPicker.delegate = self
+        timerSettingPicker.dataSource = self
+        
+        // UserDefaultsの取得
+        let setting = UserDefaults.standard
+        let timerValue = setting.integer(forKey: settingKey)
+        
+        // Pickerの選択を合わせる
+        for row in 0..<settingArray.count {
+            if settingArray[row] == timerValue {
+                timerSettingPicker.selectRow(row, inComponent: 0, animated: true)
+            }
+        }
     }
     
 
